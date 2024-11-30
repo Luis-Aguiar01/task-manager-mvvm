e91a99f9d24318b2607995a026a6aa4bca7be937
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-11-30 15:28:16 -0300

    Implementação do MainActivity e do MainViewModel

--- app/src/main/java/br/edu/ifsp/dmo1/gerenciadortarefasmvvm/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/gerenciadortarefasmvvm/ui/main/MainActivity.kt
@@ -2,11 +2,36 @@ package br.edu.ifsp.dmo1.gerenciadortarefasmvvm.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import br.edu.ifsp.dmo1.gerenciadortarefasmvvm.R
+import br.edu.ifsp.dmo1.gerenciadortarefasmvvm.databinding.ActivityMainBinding
+import br.edu.ifsp.dmo1.gerenciadortarefasmvvm.ui.adapter.TaskAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: TaskAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        configListview()
+        configObservers()
+    }
+
+    private fun configListview() {
+        adapter = TaskAdapter(this, mutableListOf())
+        binding.listTasks.adapter = adapter
+    }
+
+    private fun configObservers() {
+        viewModel.tasks.observe(this, Observer {
+            adapter.updateTasks(it)
+        })
     }
 }
